d6fa8c9b6f0dcc0b914aa27b06bc1fcd80610841
kavilee <kavilee@163.com>
2016-07-29 18:20:20 +0800

    retrofit

---
+++ app/src/main/java/com/lz/www/ambts/model/jk/IUserService.java
@@ -0,0 +1,26 @@
+package com.lz.www.ambts.model.jk;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ * Created by Administrator on 2016-07-29.
+ */
+public interface IUserService {
+
+    @GET("users/{user}/repos")
+    Call<String> getOne(@Path("user") String user);
+
+    @GET("user/getlist")
+    Call<String> getList();
+
+    @GET("user/login")
+    Call<String> login(@Query("code") String code,@Query("pwd") String pwd);
+
+    @POST("user/register")
+    Call<String> add(@Query("code") String code,@Query("pwd") String pwd,@Query("mobile") String mobile);
+
+}
