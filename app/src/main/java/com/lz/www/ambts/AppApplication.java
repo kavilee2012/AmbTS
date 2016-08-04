b338aba4864b7fe8a88c7374b6ed8847553a0fe1
lyc <lyc524@163.com>
2016-08-05 01:34:50 +0800

    uil

--- app/src/main/java/com/lz/www/ambts/AppApplication.java
+++ app/src/main/java/com/lz/www/ambts/AppApplication.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import com.lz.www.ambts.ui.component.DaggerRegisterComponent;
 import com.lz.www.ambts.ui.component.RegisterComponent;
 import com.lz.www.ambts.ui.module.RegisterModule;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
 /**
  * Created by Administrator on 2016-07-30.
@@ -22,6 +24,14 @@ public class AppApplication extends Application {
     public void onCreate() {
         super.onCreate();
         context = getApplicationContext();
+
+//        //创建默认的ImageLoader配置参数
+//        ImageLoaderConfiguration configuration = ImageLoaderConfiguration
+//                .createDefault(this);
+//
+//        //Initialize ImageLoader with configuration.
+//        ImageLoader.getInstance().init(configuration);
+
     }
 
     public AppComponent getAppComponent(){
--- app/src/main/java/com/lz/www/ambts/ui/BannerActivity.java
+++ app/src/main/java/com/lz/www/ambts/ui/BannerActivity.java
@@ -8,6 +8,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -54,6 +55,9 @@ public class BannerActivity extends AppCompatActivity {
     private View dot3;
     private View dot4;
 
+    Button btnTest;
+    ImageView imgTest;
+
     // 定时任务
     private ScheduledExecutorService scheduledExecutorService;
 
@@ -75,6 +79,9 @@ public class BannerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_banner);
 
+        btnTest=(Button)findViewById(R.id.btnLoad);
+        imgTest=(ImageView)findViewById(R.id.imgTest);
+
         // 使用ImageLoader之前初始化
         initImageLoader();
 
@@ -91,6 +98,15 @@ public class BannerActivity extends AppCompatActivity {
         initAdData();
 
         startAd();
+
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mImageLoader.displayImage("http://d.hiphotos.baidu.com/image/w%3D310/sign=54884c82b78f8c54e3d3c32e0a282dee/a686c9177f3e670932e4cf9338c79f3df9dc55f2.jpg"
+                        ,imgTest
+                        ,options);
+            }
+        });
     }
 
     /**
@@ -206,7 +222,8 @@ public class BannerActivity extends AppCompatActivity {
         adDomain.setTitle("我和令计划只是同姓");
         adDomain.setTopicFrom("阿宅");
         adDomain.setTopic("我想知道令狐安和令计划有什么关系？");
-        adDomain.setImgUrl("http://g.hiphotos.baidu.com/image/w%3D310/sign=bb99d6add2c8a786be2a4c0f5708c9c7/d50735fae6cd7b8900d74cd40c2442a7d9330e29.jpg");
+        adDomain.setImgUrl("drawable://" + R.drawable.mn1);
+//        adDomain.setImgUrl("http://g.hiphotos.baidu.com/image/w%3D310/sign=bb99d6add2c8a786be2a4c0f5708c9c7/d50735fae6cd7b8900d74cd40c2442a7d9330e29.jpg");
         adDomain.setAd(false);
         adList.add(adDomain);
 
@@ -216,8 +233,7 @@ public class BannerActivity extends AppCompatActivity {
         adDomain2.setTitle("我和令计划只是同姓");
         adDomain2.setTopicFrom("小巫");
         adDomain2.setTopic("“我想知道令狐安和令计划有什么关系？”");
-        adDomain2
-                .setImgUrl("http://g.hiphotos.baidu.com/image/w%3D310/sign=7cbcd7da78f40ad115e4c1e2672e1151/eaf81a4c510fd9f9a1edb58b262dd42a2934a45e.jpg");
+        adDomain2.setImgUrl("http://g.hiphotos.baidu.com/image/w%3D310/sign=7cbcd7da78f40ad115e4c1e2672e1151/eaf81a4c510fd9f9a1edb58b262dd42a2934a45e.jpg");
         adDomain2.setAd(false);
         adList.add(adDomain2);
 
@@ -227,8 +243,7 @@ public class BannerActivity extends AppCompatActivity {
         adDomain3.setTitle("我和令计划只是同姓");
         adDomain3.setTopicFrom("旭东");
         adDomain3.setTopic("“我想知道令狐安和令计划有什么关系？”");
-        adDomain3
-                .setImgUrl("http://e.hiphotos.baidu.com/image/w%3D310/sign=392ce7f779899e51788e3c1572a6d990/8718367adab44aed22a58aeeb11c8701a08bfbd4.jpg");
+        adDomain3.setImgUrl("http://e.hiphotos.baidu.com/image/w%3D310/sign=392ce7f779899e51788e3c1572a6d990/8718367adab44aed22a58aeeb11c8701a08bfbd4.jpg");
         adDomain3.setAd(false);
         adList.add(adDomain3);
 
@@ -238,8 +253,7 @@ public class BannerActivity extends AppCompatActivity {
         adDomain4.setTitle("我和令计划只是同姓");
         adDomain4.setTopicFrom("小软");
         adDomain4.setTopic("“我想知道令狐安和令计划有什么关系？”");
-        adDomain4
-                .setImgUrl("http://d.hiphotos.baidu.com/image/w%3D310/sign=54884c82b78f8c54e3d3c32e0a282dee/a686c9177f3e670932e4cf9338c79f3df9dc55f2.jpg");
+        adDomain4.setImgUrl("http://d.hiphotos.baidu.com/image/w%3D310/sign=54884c82b78f8c54e3d3c32e0a282dee/a686c9177f3e670932e4cf9338c79f3df9dc55f2.jpg");
         adDomain4.setAd(false);
         adList.add(adDomain4);
 
@@ -249,8 +263,7 @@ public class BannerActivity extends AppCompatActivity {
         adDomain5.setTitle("我和令计划只是同姓");
         adDomain5.setTopicFrom("大熊");
         adDomain5.setTopic("“我想知道令狐安和令计划有什么关系？”");
-        adDomain5
-                .setImgUrl("http://e.hiphotos.baidu.com/image/w%3D310/sign=66270b4fe8c4b7453494b117fffd1e78/0bd162d9f2d3572c7dad11ba8913632762d0c30d.jpg");
+        adDomain5.setImgUrl("http://e.hiphotos.baidu.com/image/w%3D310/sign=66270b4fe8c4b7453494b117fffd1e78/0bd162d9f2d3572c7dad11ba8913632762d0c30d.jpg");
         adDomain5.setAd(true); // 代表是广告
         adList.add(adDomain5);
 
